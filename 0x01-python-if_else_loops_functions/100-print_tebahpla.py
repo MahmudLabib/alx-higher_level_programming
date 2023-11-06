9a30402ade4692adfb0d1dcd050eb5dc3e762ca3
MahmudLabib <mahmud.labiib@gmail.com>
2023-11-06 12:20:38 +0200

    SCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a'), -1):
-    print(chr(i), end='')
+for i in range(ord('z'), ord('a') - 1, -1):
+    char = chr(i)
+    if (ord('z') - i) % 2 == 0:
+        char = chr(i - 32)
+    print(char, end='')
